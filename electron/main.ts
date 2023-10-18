b66774b2da882d1a792b0bd44d0dd46c7f4a4ea6
DESKTOP-87KDBD7\Jenika <jenikamacaso@gmail.com>
2023-10-18 14:48:28 +0800

    ** working store data

--- electron/main.ts
+++ electron/main.ts
@@ -1,6 +1,36 @@
-import { app, BrowserWindow, ipcMain } from 'electron';
-import { getQuery } from './query';
-import path from 'node:path';
+import { app, BrowserWindow, ipcMain } from "electron";
+import { getQuery } from "./query";
+import path from "node:path";
+import bcrypt from "bcryptjs";
+import { isLoggedIn, login, logout } from "./store";
+
+///////////////////////////////////// IPC HANDLERS //////////////////////////////
+ipcMain.on("isLoggedIn", async (event) => {
+  const isLog = await isLoggedIn();
+  console.log(isLog);
+  event.returnValue = isLog;
+});
+
+// Invoke
+ipcMain.handle("login", async (_event, args) => {
+  const q = `SELECT * FROM Users WHERE Username = '${args.username}'`;
+  const data = await getQuery(q);
+  console.log(data);
+  if (comparePassword(args?.password, data?.Password)) {
+    console.log("successful compare");
+
+    //Save to localstorage
+    return await login(data);
+  }
+});
+
+ipcMain.handle("users", async (_event: any, _args: any) => {
+  const q = `SELECT * FROM Users`;
+  const data = await getQuery(q);
+  return await login(data);
+});
+
+////////////////////////////////////////////////////////////////////////////////
 
 // The built directory structure
 //
@@ -11,57 +41,37 @@ import path from 'node:path';
 // │ │ ├── main.js
 // │ │ └── preload.js
 // │
-process.env.DIST = path.join(__dirname, '../dist');
-process.env.VITE_PUBLIC = app.isPackaged ? process.env.DIST : path.join(process.env.DIST, '../public');
-
+process.env.DIST = path.join(__dirname, "../dist");
+process.env.VITE_PUBLIC = app.isPackaged
+  ? process.env.DIST
+  : path.join(process.env.DIST, "../public");
 
 let win: BrowserWindow | null;
 // 🚧 Use ['ENV_NAME'] avoid vite:define plugin - Vite@2.x
-const VITE_DEV_SERVER_URL = process.env['VITE_DEV_SERVER_URL'];
+const VITE_DEV_SERVER_URL = process.env["VITE_DEV_SERVER_URL"];
 
 function createWindow() {
   win = new BrowserWindow({
-    icon: path.join(process.env.VITE_PUBLIC, 'electron-vite.svg'),
+    icon: path.join(process.env.VITE_PUBLIC, "electron-vite.svg"),
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
-      sandbox: false
+      preload: path.join(__dirname, "preload.js"),
+      sandbox: false,
     },
     width: 900,
-    height: 900
+    height: 900,
   });
 
   // Test active push message to Renderer-process.
-  win.webContents.on('did-finish-load', () => {
-    win?.webContents.send('main-process-message', (new Date).toLocaleString());
+  win.webContents.on("did-finish-load", () => {
+    win?.webContents.send("main-process-message", new Date().toLocaleString());
   });
 
   if (VITE_DEV_SERVER_URL) {
     win.loadURL(VITE_DEV_SERVER_URL);
   } else {
     // win.loadFile('dist/index.html')
-    win.loadFile(path.join(process.env.DIST, 'index.html'));
+    win.loadFile(path.join(process.env.DIST, "index.html"));
   }
-
-  ///////////////////////////////////// IPC HANDLERS //////////////////////////////
-  // ipcMain.on("get/user", async (event, args) => {
-  //   const user = await getUser();
-  //   event.returnValue = user;
-  // });
-
-  // Invoke
-  ipcMain.handle("login", async (_event, args) => {
-    const q = `SELECT * FROM Users WHERE Username = '${args.username}'`;
-    const data = await getQuery(q);
-    return data;
-  });
-
-  // Invoke
-  ipcMain.handle("users", async (_event: any, _args: any) => {
-    const q = `SELECT * FROM Users`;
-    const data = await getQuery(q);
-    console.log(data);
-    return data;
-  });
 }
 
 ///////////////////////////////////////////////////////////////////////////////////
@@ -69,14 +79,15 @@ function createWindow() {
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+app.on("window-all-closed", async () => {
+  if (process.platform !== "darwin") {
+    await logout();
     app.quit();
     win = null;
   }
 });
 
-app.on('activate', () => {
+app.on("activate", () => {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
@@ -85,3 +96,17 @@ app.on('activate', () => {
 });
 
 app.whenReady().then(createWindow);
+
+const comparePassword = (password: string, hash: string) => {
+  try {
+    // Compare password
+    console.log("password: " + password);
+    console.log("hash: " + hash);
+    return bcrypt?.compareSync(password, hash);
+  } catch (error) {
+    console.log(error);
+  }
+
+  // Return false if error
+  return false;
+};
--- electron/store.ts
+++ electron/store.ts
@@ -3,29 +3,22 @@ import Store from "electron-store";
 const store = new Store();
 
 const isLoggedIn = async (): Promise<any> => {
-    return store.get("isLoggedIn");
+  return store.get("isLoggedIn");
 };
 
 const login = async (user: string) => {
-    console.log("Login!");
-    store.set("isLoggedIn", true);
-    store.set("user", user);
-    return true;
+  store.set("isLoggedIn", true);
+  store.set("user", user);
+  return true;
 };
 
 const logout = async () => {
-    console.log("Logout!");
-    store.delete("isLoggedIn");
-    store.delete("user");
+  store.delete("isLoggedIn");
+  store.delete("user");
 };
 
 const getUser = async (): Promise<any> => {
-    return store.get("user");
+  return store.get("user");
 };
 
-export {
-    isLoggedIn,
-    login,
-    logout,
-    getUser
-};
+export { isLoggedIn, login, logout, getUser };
