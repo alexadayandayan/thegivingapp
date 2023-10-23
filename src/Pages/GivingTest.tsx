8e0d74405d1b01a6571ede70f6fa06e683b7192c
DESKTOP-87KDBD7\Jenika <jenikamacaso@gmail.com>
2023-10-24 07:37:30 +0800

    ** wip

--- src/Pages/GivingTest.tsx
+++ src/Pages/GivingTest.tsx
@@ -1,118 +1,267 @@
-import DashboardSidebar from "@/Components/DashboardSidebar";
-import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import { Button, Grid, Icon, Table, Header, Image } from "semantic-ui-react";
+import DashboardSidebar from "../Components/DashboardSidebar";
+import React, { ChangeEvent, FormEvent, useEffect, useState } from "react";
+import { Button, Grid, Icon, Table, Form, Dropdown } from "semantic-ui-react";
+import { useNavigate } from "react-router";
+import { IMember } from "../Data/member";
+import { IOfferingFormState } from "../Data/giving";
 
 const GivingTest: React.FC = () => {
-  const [offerings, setOfferings] = useState<any[]>([]);
+  const navigate = useNavigate();
+  let memberId: string = "";
+  const [allMembers, setAllMembers] = useState<any[]>([]);
+  const [formData, setFormData] = useState<IOfferingFormState | any>({
+    bestGift: 0,
+    buildingFund: 0,
+    childrensMinistry: 0,
+    dance: 0,
+    fEBC700: 0,
+    flowerOrPlants: 0,
+    meralco: 0,
+    music: 0,
+    giftForPastor: 0,
+    giftForBrother: 0,
+    others: "",
+    tithe: 0,
+    total: 0,
+    youth: 0,
+    firstname: null,
+    lastname: null,
+    gender: null,
+  });
 
-  const getOfferings = async () => {
-    const giving = await window.api.getOfferings();
-    console.log(giving);
-    setOfferings(giving);
+  const getAllMembers = async () => {
+    const members = (await window.api.getMembers()) as IMember | any;
+    const options: any = members?.map((m: any) => ({
+      key: m.Id,
+      text: `${m.Firstname} ${m.Lastname}`,
+      value: m.Id,
+    }));
+    setAllMembers(options);
+  };
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleMemberChange = (
+    _e: React.SyntheticEvent<HTMLElement, Event>,
+    data: any
+  ) => {
+    setFormData({
+      ...formData,
+      memberId: data.value,
+    });
+  };
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    const createGiving = await window.api.createOffering(formData);
+    if (createGiving !== "Success") {
+      return;
+    }
+    navigate("/giving");
   };
 
   useEffect(() => {
-    getOfferings();
+    getAllMembers();
   }, []);
-
   return (
     <>
-      <Grid columns="equal">
-        <Grid.Column>
-          <DashboardSidebar />
-        </Grid.Column>
+      <Form>
+        <Grid columns="equal">
+          <Grid.Column>
+            <DashboardSidebar />
+          </Grid.Column>
 
-        <Grid.Column width={13}>
-          <div className="header-block">
-            <Grid columns="equal">
-              <Grid.Column>
-                <h3>Giving for Today</h3>
-              </Grid.Column>
-              <Grid.Column>
-                <Button
-                  floated="right"
-                  icon
-                  labelPosition="left"
-                  primary
-                  size="small"
-                >
-                  <Icon name="like" /> Add Giving Entry
-                </Button>
-              </Grid.Column>
-            </Grid>
-          </div>
-          <div className="overflow-x">
-            <Table size="large" singleLine columns={15} celled selectable>
-              <Table.Header>
-                <Table.Row>
-                  <Table.HeaderCell>Name</Table.HeaderCell>
-                  <Table.HeaderCell>Tithe</Table.HeaderCell>
-                  <Table.HeaderCell>Building Fund</Table.HeaderCell>
-                  <Table.HeaderCell>Best Gift</Table.HeaderCell>
-                  <Table.HeaderCell>Love Gifts</Table.HeaderCell>
-                  <Table.HeaderCell>SS</Table.HeaderCell>
-                  <Table.HeaderCell>Youth</Table.HeaderCell>
-                  <Table.HeaderCell>Flower Or Plants</Table.HeaderCell>
-                  <Table.HeaderCell>FEBC 700</Table.HeaderCell>
-                  <Table.HeaderCell>Dance</Table.HeaderCell>
-                  <Table.HeaderCell>Music</Table.HeaderCell>
-                  <Table.HeaderCell>Meralco</Table.HeaderCell>
-                  <Table.HeaderCell>Others</Table.HeaderCell>
-                  <Table.HeaderCell>Total</Table.HeaderCell>
-                  <Table.HeaderCell colSpan="2">Actions</Table.HeaderCell>
-                </Table.Row>
-              </Table.Header>
+          <Grid.Column width={13}>
+            <div className="header-block">
+              <Grid columns="equal">
+                <Grid.Column>
+                  <h3>New Giving Entry</h3>
+                </Grid.Column>
+              </Grid>
+            </div>
+            <div className="overflow-x">
+              <Table size="large" singleLine columns={15} celled selectable>
+                <Table.Header>
+                  <Table.Row>
+                    <Table.HeaderCell>Name</Table.HeaderCell>
+                    <Table.HeaderCell>Tithe</Table.HeaderCell>
+                    <Table.HeaderCell>Building Fund</Table.HeaderCell>
+                    <Table.HeaderCell>Best Gift</Table.HeaderCell>
+                    <Table.HeaderCell>FEBC 700</Table.HeaderCell>
+                    <Table.HeaderCell>Gift for Pastor</Table.HeaderCell>
+                    <Table.HeaderCell>Gift for Bro/Sis</Table.HeaderCell>
+                    <Table.HeaderCell>Children's Ministry</Table.HeaderCell>
+                    <Table.HeaderCell>Flower/Plants</Table.HeaderCell>
+                    <Table.HeaderCell>L&S Youth</Table.HeaderCell>
+                    <Table.HeaderCell>Dance</Table.HeaderCell>
+                    <Table.HeaderCell>Meralco/Maynilad</Table.HeaderCell>
+                    <Table.HeaderCell>Music</Table.HeaderCell>
+                    <Table.HeaderCell>Others</Table.HeaderCell>
+                    <Table.HeaderCell>Total</Table.HeaderCell>
+                  </Table.Row>
+                </Table.Header>
+                <Table.Body>
+                  <Table.Row>
+                    <Table.Cell>
+                      <Dropdown
+                        placeholder="Select Member"
+                        fluid
+                        selection
+                        options={allMembers}
+                        className="dropdown"
+                        onChange={handleMemberChange}
+                      />
+                    </Table.Cell>
+                    <Table.Cell>
+                      <Form.Input
+                        name="tithe"
+                        type="number"
+                        value={formData.tithe}
+                        onChange={handleChange}
+                      />
+                    </Table.Cell>
+                    <Table.Cell>
+                      <Form.Input
+                        name="buildingFund"
+                        type="number"
+                        value={formData.buildingFund}
+                        onChange={handleChange}
+                      />
+                    </Table.Cell>
+                    <Table.Cell>
+                      <Form.Input
+                        name="bestGift"
+                        type="number"
+                        value={formData.bestGift}
+                        onChange={handleChange}
+                      />
+                    </Table.Cell>
+                    <Table.Cell>
+                      <Form.Input
+                        name="fEBC700"
+                        type="number"
+                        value={formData.fEBC700}
+                        onChange={handleChange}
+                      />
+                    </Table.Cell>
+                    <Table.Cell>
+                      <Form.Input
+                        name="giftForPastor"
+                        type="number"
+                        value={formData.giftForPastor}
+                        onChange={handleChange}
+                      />
+                    </Table.Cell>
+                    <Table.Cell>
+                      <Form.Input
+                        name="giftForBrother"
+                        type="number"
+                        value={formData.giftForBrother}
+                        onChange={handleChange}
+                      />
+                    </Table.Cell>
+                    <Table.Cell>
+                      <Form.Input
+                        name="childrensMinistry"
+                        type="number"
+                        value={formData.childrensMinistry}
+                        onChange={handleChange}
+                      />
+                    </Table.Cell>
+                    <Table.Cell>
+                      <Form.Input
+                        name="flowerOrPlants"
+                        type="number"
+                        value={formData.flowerOrPlants}
+                        onChange={handleChange}
+                      />
+                    </Table.Cell>
+                    <Table.Cell>
+                      <Form.Input
+                        name="youth"
+                        type="number"
+                        value={formData.youth}
+                        onChange={handleChange}
+                      />
+                    </Table.Cell>
+                    <Table.Cell>
+                      <Form.Input
+                        name="dance"
+                        type="number"
+                        value={formData.dance}
+                        onChange={handleChange}
+                      />
+                    </Table.Cell>
+                    <Table.Cell>
+                      <Form.Input
+                        name="meralco"
+                        type="number"
+                        value={formData.meralco}
+                        onChange={handleChange}
+                      />
+                    </Table.Cell>
+                    <Table.Cell>
+                      <Form.Input
+                        name="music"
+                        type="number"
+                        value={formData.music}
+                        onChange={handleChange}
+                      />
+                    </Table.Cell>
+                    <Table.Cell>
+                      <Form.Input
+                        name="others"
+                        type="number"
+                        value={formData.others}
+                        onChange={handleChange}
+                      />
+                    </Table.Cell>
+                    <Table.Cell>
+                      <Form.Input
+                        name="total"
+                        type="number"
+                        value={formData.total}
+                        onChange={handleChange}
+                      />
+                    </Table.Cell>
+                  </Table.Row>
+                </Table.Body>
 
-              <Table.Body>
-                {offerings.length
-                  ? offerings.map((member) => (
-                      <Table.Row key={"uniqueId" + member.Giving}>
-                        <Table.Cell>
-                          <Header as="h4" image>
-                            {member.Gender === "female" ? (
-                              <Image
-                                src="https://react.semantic-ui.com/images/avatar/small/lena.png"
-                                rounded
-                                size="mini"
-                              />
-                            ) : (
-                              <Image
-                                src="https://react.semantic-ui.com/images/avatar/small/matthew.png"
-                                rounded
-                                size="mini"
-                              />
-                            )}
-                            <Header.Content>{member.Firstname}</Header.Content>
-                          </Header>
-                        </Table.Cell>
-                        <Table.Cell></Table.Cell>
-                        <Table.Cell></Table.Cell>
-                        <Table.Cell></Table.Cell>
-                        <Table.Cell></Table.Cell>
-                        <Table.Cell></Table.Cell>
-                        <Table.Cell></Table.Cell>
-                        <Table.Cell></Table.Cell>
-                        <Table.Cell></Table.Cell>
-                        <Table.Cell></Table.Cell>
-                        <Table.Cell></Table.Cell>
-                        <Table.Cell></Table.Cell>
-                        <Table.Cell></Table.Cell>
-                        <Table.Cell></Table.Cell>
-                        <Table.Cell selectable positive>
-                          <Link to="/giving-edit">Edit</Link>
-                        </Table.Cell>
-                        <Table.Cell selectable negative>
-                          <Link to="#">Delete</Link>
-                        </Table.Cell>
-                      </Table.Row>
-                    ))
-                  : null}
-              </Table.Body>
-            </Table>
-          </div>
-        </Grid.Column>
-      </Grid>
+                <Table.Footer fullWidth>
+                  <Table.Row>
+                    <Table.HeaderCell colSpan="16">
+                      <Button
+                        floated="right"
+                        icon
+                        labelPosition="left"
+                        primary
+                        size="small"
+                        onClick={handleSubmit}
+                      >
+                        <Icon name="like" /> Save
+                      </Button>
+                      <Button
+                        floated="right"
+                        icon
+                        labelPosition="left"
+                        size="small"
+                        onClick={handleSubmit}
+                      >
+                        <Icon name="cancel" /> Cancel
+                      </Button>
+                    </Table.HeaderCell>
+                  </Table.Row>
+                </Table.Footer>
+              </Table>
+            </div>
+          </Grid.Column>
+        </Grid>
+      </Form>
     </>
   );
 };
